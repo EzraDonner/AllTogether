cae0a3554553b820a2a23e29607f0d82958133e0
Ezra Donner <pianist4486@yahoo.com>
2025-01-30 11:13:34 -0500

    all users endpoint created

--- app.js
+++ app.js
@@ -106,6 +106,20 @@ app.get("/api/users/aboutMe", verifyToken, async (req, res, next) => {
   });
 });
 
+//Endpoint to get all users
+app.get("/api/users/all", verifyToken, async (req, res, next) => {
+  try {
+    const users = await prisma.user.findMany({
+      select: { id: true, firstName: true, lastName: true, email: true }, // Exclude passwords
+    });
+
+    res.status(200).json(users);
+  } catch (error) {
+    console.error("Error fetching users:", error);
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
 module.exports = router;
 // Start the server
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
